95fca0ae6456ff1ebbefa257f165d6d5fee7b4d8
Thibaut Baguette <github@thibautbaguette.com>
2025-11-30 17:19:26 -0500

    Setup 2025 and test with 2015 day 1

---
+++ 2015/src/day1.rs
@@ -0,0 +1,57 @@
+use aoc_runner_derive::aoc;
+
+#[aoc(day1, part1)]
+pub fn solve_part1(input: &str) -> i32 {
+    input
+        .chars()
+        .map(|c| match c {
+            '(' => 1,
+            ')' => -1,
+            _ => panic!("unknown character"),
+        })
+        .sum()
+}
+
+#[aoc(day1, part2)]
+pub fn solve_part2(input: &str) -> usize {
+    let mut floor = 0;
+    for (idx, c) in input.chars().enumerate() {
+        floor += match c {
+            '(' => 1,
+            ')' => -1,
+            _ => panic!("unknown character"),
+        };
+        if floor == -1 {
+            return idx + 1;
+        }
+    }
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    use super::*;
+
+    #[rstest]
+    #[case("(())", 0)]
+    #[case("()()", 0)]
+    #[case("(((", 3)]
+    #[case("(()(()(", 3)]
+    #[case("))(((((", 3)]
+    #[case("())", -1)]
+    #[case("))(", -1)]
+    #[case(")))", -3)]
+    #[case(")())())", -3)]
+    fn test_day1_part1(#[case] input: &str, #[case] expected: i32) {
+        assert_eq!(solve_part1(input), expected);
+    }
+
+    #[rstest]
+    #[case(")", 1)]
+    #[case("()())", 5)]
+    fn test_day1_part2(#[case] input: &str, #[case] expected: usize) {
+        assert_eq!(solve_part2(input), expected);
+    }
+}
---
+++ 2025/src/day1.rs
@@ -0,0 +1,11 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day1)]
+pub fn input_generator(_input: &str) -> () {
+    todo!()
+}
+
+#[aoc(day1, part1)]
+pub fn solve_part1(_input: &()) -> u32 {
+    todo!()
+}
